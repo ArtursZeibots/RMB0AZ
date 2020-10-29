2d2988b44ddccdd1a8a24f9cde40d259ece06ffa
ArtursZeibots <71380674+ArtursZeibots@users.noreply.github.com>
2020-10-29 16:49:01 +0200

    Update factorialWhile.c

--- factorialWhile.c
+++ factorialWhile.c
@@ -3,30 +3,30 @@
 void main()
 {
     long long input_num;
-    char charNum;
-    long long longNum;
-    int intNum;
+    char charNum = 1;
+    long long longNum = 1;
+    int intNum = 1;
     char choice;
-    long long reference;
+    long long reference = 1;
     printf("Lūdzu ievadiet decimālu skaitli: ");
     scanf("%lld99", &input_num);
     printf("Lūdzu ievadiet tā datu tipa pirmo burtu, kādā vēlaties iegūt rezultātu: ");
     scanf(" %c99", &choice);
     int counter = 1;
-    long long result_reference;
-    while(counter <= input_num)
+    long long result_reference = 1;
+    while(counter < input_num)
     {
-        result_reference *= counter * (counter + 1);
+        result_reference *= (counter + 1);
         counter++;
     }
     counter = 1;
     if(choice == 'c')
     {
-        while(counter <= input_num)
+        while(counter < input_num)
         {
         
-            charNum *= counter * (counter + 1);
-            reference *= counter * (counter + 1);
+            charNum *= (counter + 1);
+            reference *= (counter + 1);
             counter++;
             if(charNum != reference){
                 break;
@@ -47,11 +47,11 @@ void main()
     reference = input_num;
     }else if(choice == 'i')
     {
-        while(counter <= input_num)
+        while(counter < input_num)
         {
         
-            intNum *= counter * (counter + 1);
-            reference *= counter * (counter + 1);
+            intNum *= (counter + 1);
+            reference *= (counter + 1);
             counter++;
             if(intNum != reference){
                 break;
@@ -71,11 +71,12 @@ void main()
     reference = input_num;
     }else if(choice == 'l')
     {
-        while(counter <= input_num)
+        while(counter < input_num)
         {
         
-            longNum *= counter * (counter + 1);
-            reference *= counter * (counter + 1);
+            longNum *= (counter + 1);
+            printf("%lld\n", longNum);
+            reference *= (counter + 1);
             counter++;
             if(longNum != reference){
                 break;
