31bf08ce14d677176e0773e29405521986aa905c
ArtursZeibots <71380674+ArtursZeibots@users.noreply.github.com>
2021-01-11 21:18:18 +0200

    Create code_LD1.c

---
+++ LD1/code_LD1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+ double x,y;
+ printf("Šī programma aprēķina cosh no x/2 funkcijas vērtības izmantojot Teilora rindu.\n");
+ printf("Lūdzu ievadiet x: ");
+ scanf(" %lf99", &x);
+
+ FILE * printFile;
+ printFile = fopen("cosh_dati.txt","w");
+ fprintf(printFile,"x=%f             ", x);
+ x=x/2;
+ y=cosh(x);
+ fprintf(printFile,"y=%f\n", y);
+ fprintf(printFile,"###################\n");
+
+
+ long double z=1.0;
+ long double s=1.0;
+
+ for(int a = 1; a<=500; a++)
+ {
+  z=z* pow(x,2)/(2*a*(2*a-1));
+  s+=z;
+  fprintf(printFile,"%d. %Lf     %Lf\n", a, z, s);
+  if((z/pow(x,2)/(2*a*(2*a-1)) == z))
+  {
+   break;
+  }
+
+ }
+
+ fclose(printFile);
+}
+
